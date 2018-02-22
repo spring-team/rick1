71554b3911f4d945edaededb134cabd9a2c8dcc5
Rod Johnson <johnsonroda@gmail.com>
2018-02-22 12:39:43 -0800

    Initial commit from Atomist

---
+++ src/main/java/com/atomist/rick/Rick1Application.java
@@ -0,0 +1,12 @@
+package com.atomist.rick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rick1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rick1Application.class, args);
+	}
+}
